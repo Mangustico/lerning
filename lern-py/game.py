244647aa0b61b1c4e2c63a3300c3f21f86ed77e5
Mangustico <dmitrijkuvikov87@gmail.com>
2026-07-07 15:58:15 +0300

    исправил игру

--- lern-py/game.py
+++ lern-py/game.py
@@ -5,29 +5,23 @@ secret_number = random.randint(1, 100)
 attempt = 0
 
 res = 1
-command = int(input("Guess the number from 1 to 100: "))
-attempt +=1
-if command < secret_number:
-    print("its less than secret number")
-elif command > secret_number:
-    print("its bigger than secret number")
-else:
-    print("ugadamnright")
-    sys.exit()
+print("Guess the number from 1 to 100: ")
 
 while res != 0 and attempt != 15:
-    command = int(input("New guess: "))
-    attempt += 1
+    try:
+        command = int(input("Your guess: "))
+        attempt += 1
 
-    if command < secret_number:
-        print("its less than secret number")
-    elif command > secret_number:
-        print("its bigger than secret number")
-    elif command == secret_number:
-        res = 0
-    else:
-        print("only integer")
+        if command < secret_number:
+            print("its less than secret number")
+        elif command > secret_number:
+            print("its bigger than secret number")
+        elif command == secret_number:
+            res = 0
+    except ValueError:
+        print("Input only integer, try again")
+
 if res == 0:
     sys.exit(f"ouright, the number is {secret_number} and you got it in {attempt} attempts")
 
-else: print("you lose")
+else: sys.exit("you lose")
