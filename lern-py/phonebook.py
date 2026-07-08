128baaf701b9e4b4767bd0b018e0551bce09c4f9
Mangustico <dmitrijkuvikov87@gmail.com>
2026-07-08 14:02:57 +0300

    Create phonebook.py

---
+++ lern-py/phonebook.py
@@ -0,0 +1,36 @@
+import sys
+
+phonebook = {}
+
+print("Commands:\n")
+command = 0
+while True:
+    try:
+        command = int(input("1 - add contact, 2 - found, 3 - show contact list, 4 - delete contact, 5 - exit\n"))
+    except ValueError:
+        print("Only announced commands is available\n")
+        continue
+    else:
+        if command == 1:
+            phonebook[str(input("Input new contact's name: "))] = str(input("New contact's number: "))
+            print("Contact added")
+        elif command == 2:
+            request_name = str(input("Input contact's name: "))
+            if request_name in phonebook:
+                print(f"Name: {request_name}, number: {phonebook[request_name]}")
+            else:
+                print("There is no contact with this name")
+        elif command == 3:
+            for name in phonebook:
+                print(f"Name: {name} , number: {phonebook[name]}")
+        elif command == 4:
+            target = str(input("What contact you wanna delete: "))
+            try:
+                del phonebook[target]
+            except KeyError:
+                print("Contact with this name is not exist")
+            else:
+                print("Contact successfully deleted")
+        else:
+            sys.exit("\nProgram is end")
+
