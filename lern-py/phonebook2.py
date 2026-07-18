b93ac936de784d3c58a1f24d10180c28170d55dc
Mangustico <dmitrijkuvikov87@gmail.com>
2026-07-18 20:15:45 +0300

    сделал рефакторинг

--- lern-py/phonebook2.py
+++ lern-py/phonebook2.py
@@ -1,47 +1,66 @@
 import sys
 import json
 
-try:
-    with open("list.json", 'r') as f_out:
-        phonebook = json.load(f_out)
-except FileNotFoundError:
-    phonebook = {}
-
-print("Commands:")
-command = 0
-while True:
+def load_phonebook(file_name):
     try:
-        command = int(input("1 - add contact, 2 - find, 3 - show contact list, 4 - delete contact, 5 - exit\n"))
-    except ValueError:
-        print("Only announced commands are available\n")
-        continue
+        with open(file_name, 'r') as f_in:
+            return json.load(f_in)
+    except FileNotFoundError:
+        return {}
+
+def add_contact(phonebook):
+    phonebook[input("Input new contact's name: ")] = input("New contact's number: ")
+    print("Contact added")
+
+def find_contact(phonebook):
+    request_name = str(input("Input contact's name: "))
+    result = [name for name in phonebook if request_name.lower() in name.lower()]
+    if result:
+        for x in result:
+            print(f"Name: {x}, number: {phonebook[x]}")
     else:
-        if command == 1:
-            phonebook[input("Input new contact's name: ")] = input("New contact's number: ")
-            print("Contact added")
-        elif command == 2:
-            request_name = str(input("Input contact's name: "))
-            result = [name for name in phonebook if request_name.lower() in name.lower()]
-            if result:
-                for x in result:
-                    print(f"Name: {x}, number: {phonebook[x]}")
-            else:
-                print("This contact is not exist")
-        elif command == 3:
-            for name in phonebook:
-                print(f"Name: {name} , number: {phonebook[name]}")
-        elif command == 4:
-            target = input("What contact you wanna delete: ")
-            try:
-                del phonebook[target]
-            except KeyError:
-                print("This contact is not exist")
-            else:
-                print("Contact successfully deleted")
-        elif command == 5:
-            with open('list.json', 'w') as f_in:
-                json.dump(phonebook, f_in, indent=3, separators=(',', ':'))
-            print("\nProgram is finished")
-            break
-        else:
+        print("This contact is not exist")
+
+def show_list(phonebook):
+    for name in phonebook:
+        print(f"Name: {name} , number: {phonebook[name]}")
+
+def delete_contact(phonebook):
+    target = input("What contact you wanna delete: ")
+    try:
+        del phonebook[target]
+    except KeyError:
+        print("This contact is not exist")
+    else:
+        print("Contact successfully deleted")
+
+
+
+def main():
+    phonebook = load_phonebook("list.json")
+    print("Commands:")
+    while True:
+        try:
+            command = int(input("1 - add contact, 2 - find, 3 - show contact list, 4 - delete contact, 5 - exit\n"))
+        except ValueError:
             print("Only announced commands are available\n")
+            continue
+        else:
+            if command == 1:
+                add_contact(phonebook)
+            elif command == 2:
+                find_contact(phonebook)
+            elif command == 3:
+                show_list(phonebook)
+            elif command == 4:
+                delete_contact(phonebook)
+            elif command == 5:
+                with open('list.json', 'w') as f_out:
+                    json.dump(phonebook, f_out, indent=3, separators=(',', ':'))
+                print("\nProgram is finished")
+                break
+            else:
+                print("Only announced commands are available\n")
+
+if __name__ == "__main__":
+    main()
