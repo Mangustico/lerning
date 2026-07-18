5ee1c7047e9a193732fadba7f0887db69e46ec71
Mangustico <dmitrijkuvikov87@gmail.com>
2026-07-18 22:11:11 +0300

    интегрировал метод

--- lern-py/phonebook3.py
+++ lern-py/phonebook3.py
@@ -24,9 +24,13 @@ class Phonebook:
         self.contacts.append(Contact(name, number))
 
     def find_contact(self, request_name):
+        found = 1
         for c in self.contacts:
             if request_name in c.name:
                 print(f"Name: {c.name}, number: {c.number}")
+                found = 0
+        if found == 1:
+            print("This contact does not exist")
 
     def show_contacts(self):
         for c in self.contacts:
@@ -41,3 +45,37 @@ class Phonebook:
             temp_dict.update(c.to_dict())
         with open(file_name, "w") as f:
             json.dump(temp_dict, f, indent=3, separators=(',', ':'))
+
+def main():
+    phonebook = Phonebook("list.json")
+    print("Commands:")
+    while True:
+        try:
+            command = int(input("1 - add contact, 2 - find, 3 - show contact list, 4 - delete contact, 5 - exit\n"))
+        except ValueError:
+            print("Only listed commands are available\n")
+            continue
+        else:
+            if command == 1:
+                req_number = input("Input new contact's number: ")
+                req_name = input("Input new contact's name: ")
+                phonebook.add_contact(req_name, req_number)
+                phonebook.save("list.json")
+            elif command == 2:
+                req_name = input("Input contact's name: ")
+                phonebook.find_contact(req_name)
+            elif command == 3:
+                phonebook.show_contacts()
+            elif command == 4:
+                req_name = input("Which contact do you want to delete: ")
+                phonebook.delete_contact(req_name)
+                phonebook.save("list.json")
+            elif command == 5:
+                phonebook.save("list.json")
+                print("\nProgram is finished")
+                break
+            else:
+                print("Only listed commands are available\n")
+
+if __name__ == "__main__":
+    main()
